3fc7b454f35b50baa0bf87dc031b0553adcef88d
Antonino Cangialosi <ninocangialosi@yahoo.it>
2025-11-24 22:46:21 +0100

    Update binary tree

--- utils/binary_search_tree.py
+++ utils/binary_search_tree.py
@@ -1,31 +1,50 @@
+from utils.tree_node import TreeNode 
+
 class BST:
   def __init__(self):
     self.root = None
 
-  def insert(self, data):
-    new_node = TreeNode(data)
-    # Check if the BST is empty
-    if self.root ==None:
-      self.root = new_node
-      return
-    else:
-      current_node = self.root
-      while True:
-        # Check if the data to insert is smaller than the current node's data
-        if data < current_node:
-          if current_node.left_child == None:
-            current_node.left_child = new_node
-            return 
-          else:
-            current_node = current_node.left_child
-        # Check if the data to insert is greater than the current node's data
-        elif data > current_node:
-          if current_node.right_child == None:
-            current_node.right_child = new_node
+    def insert_at_beginning(self,data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
             return
-          else:
-            current_node = current_node.right_child
+        self.tail = new_node      
+        self.head = new_node
 
+    def insert(self, data):
+        new_node = TreeNode(data)
+        # Check if the BST is empty
+        if self.root is None:
+            self.root = new_node
+            return
+        else:
+            current_node = self.root
+            while True:
+                if data < current_node:
+                    if current_node.left_child is None:
+                        current_node.left_child = new_node
+                        return 
+                    else:
+                        current_node = current_node.left_child
+                elif data > current_node:
+                    if current_node.right_child is None:
+                        current_node.right_child = new_node
+                        return
+                    else:
+                        current_node = current_node.right_child
+                else:
+                    self.insert_at_beginning(current_node.data)
+
+    def search(self, data):
+        current_node = self.head
+        while current_node:
+            if current_node.data == data:
+                return True
+            else:
+                current_node = current_node.next
+        return False
 
     def find_min(self):
         current_node = self.root
