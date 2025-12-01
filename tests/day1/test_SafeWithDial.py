a553231e8f1b1952063a44fe81af642f06650279
Antonino Cangialosi <antonino.cangialosi@agilelab.it>
2025-12-01 18:01:56 +0100

    Submit solution to second part

--- tests/day1/test_SafeWithDial.py
+++ tests/day1/test_SafeWithDial.py
@@ -4,7 +4,7 @@ from .test_main import provide_test_lines
 
 import pytest
 
-from src.day1.main import SafeWithDial
+from src.day1.main import SafeWithDial, SafeWithDialPassByZeroDetector
 
 def test_should_rotate_left():
     safe = SafeWithDial()
@@ -31,4 +31,9 @@ def test_should_execute_batch(provide_test_lines):
     assert actual.dial_number == expected
     assert actual.visited_zero == 3
 
-
+def test_should_execute_batch_with_pass_by_zero(provide_test_lines):
+    safe = SafeWithDialPassByZeroDetector()
+    expected = 32
+    actual = safe.batch(provide_test_lines)
+    assert actual.dial_number == expected
+    assert actual.visited_zero == 6
