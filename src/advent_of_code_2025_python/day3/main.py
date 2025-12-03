e9c951d4c7d70206c874cb00a5ffceaca63b2795
Antonino Cangialosi <antonino.cangialosi@agilelab.it>
2025-12-03 06:55:19 +0100

    Submit solution to first part

--- src/advent_of_code_2025_python/day3/main.py
+++ src/advent_of_code_2025_python/day3/main.py
@@ -1,5 +1,24 @@
+from copy import deepcopy
 from pathlib import Path
-from typing import Optional
+from typing import Optional, List
+
+
+class BatteryBank:
+
+    def __init__(self, line: str):
+        self.batteries = [int(item) for item in line]
+        self.max_index = len(self.batteries) - 1
+
+    @property
+    def max_joltage(self) -> int:
+        max1 = max(self.batteries)
+        index1 = self.batteries.index(max1)
+        if index1 == self.max_index:
+            max2 = max(self.batteries[:index1] + self.batteries[index1 + 1:])
+            return int(f"{max2}{max1}")
+        sub_max = max(self.batteries[index1+1:])
+        return int(f"{max1}{sub_max}")
+
 
 class Solution:
     _STANDARD_PATH = Path(__file__).parent / "input.txt"
@@ -9,7 +28,8 @@ class Solution:
             self.lines = file.readlines() if not lines else lines
 
     def first_task(self) -> int:
-        pass
+        banks = [BatteryBank(line.strip("\n")) for line in self.lines]
+        return sum([bank.max_joltage for bank in banks])
 
 
     def second_task(self) -> int:
--- tests/python/day3/test_main.py
+++ tests/python/day3/test_main.py
@@ -5,8 +5,7 @@ from pathlib import Path
 
 import pytest
 
-from src.advent_of_code_2025_python.day3.main import Solution
-
+from src.advent_of_code_2025_python.day3.main import Solution, BatteryBank
 
 
 @pytest.fixture
@@ -18,11 +17,19 @@ def provide_test_lines() -> list[str]:
 
 
 
-def test_first_task(provide_test_lines: list[str]) -> None:
+def test_first_task(provide_test_lines) -> None:
     under_test = Solution(lines=provide_test_lines)
     expected = 357
     actual = under_test.first_task()
     assert actual == expected
+
+
+def test_max_joltage(provide_test_lines):
+    expectations = [98, 89, 78, 92]
+    under_test = BatteryBank
+    for ind, line in enumerate(provide_test_lines):
+        actual = under_test(line).max_joltage
+        assert actual == expectations[ind], f"Fails on line {ind}: {line}"
 
 
 def test_second_task(provide_test_lines: list[str]) -> None:
