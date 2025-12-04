99354e9a0fded9bc560899d5a338b3525a4089a8
Antonino Cangialosi <antonino.cangialosi@agilelab.it>
2025-12-04 06:06:49 +0100

    chore: Add prompt for day 4

---
+++ src/advent_of_code_2025_python/day4/main.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from typing import Optional
+
+class Solution:
+    _STANDARD_PATH = Path(__file__).parent / "input.txt"
+
+    def __init__(self, path: Path=_STANDARD_PATH, lines: Optional[list[str]] = None):
+        with open(path) as file:
+            self.lines = file.readlines() if not lines else lines
+
+    def first_task(self) -> int:
+        pass
+
+
+    def second_task(self) -> int:
+        pass
+
+def main():
+    solution = Solution()
+    print("The first answer is", solution.first_task())
+    print("The second answer is", solution.second_task())
+
+if __name__ == "__main__":
+    main()
