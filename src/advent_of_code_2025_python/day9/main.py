09c3923d83df2a4b690343bb65460c9a6adca752
Antonino Cangialosi <antonino.cangialosi@agilelab.it>
2025-12-09 06:48:47 +0100

    Submit solution to first part

--- src/advent_of_code_2025_python/day9/main.py
+++ src/advent_of_code_2025_python/day9/main.py
@@ -1,5 +1,48 @@
+import re
+from itertools import product
 from pathlib import Path
-from typing import Optional
+from typing import Optional, NamedTuple
+
+
+
+class RedTile(NamedTuple):
+    x: int
+    y: int
+
+
+class TileFloor:
+    PATTERN = re.compile(r'(\d+)')
+
+    def __init__(self, lines: list[str]):
+        self.red_tiles = [
+            RedTile(*[
+                int(el)
+                for el in re.findall(self.PATTERN, line)
+            ]) for line in lines
+        ]
+
+    @property
+    def max_area(self, strategy: str = "brute_force") -> int:
+        """ Brute force """
+        if strategy != "brute_force":
+            raise NotImplementedError(f"Strategy {strategy} was not implemented yet")
+        return self._brute_force_max_area()
+
+    def _brute_force_max_area(self) -> int:
+        max_area = 0
+        for i, j in product(range(len(self.red_tiles)), range(1, len(self.red_tiles))):
+            base = (
+                max(self.red_tiles[i].x, self.red_tiles[j].x) -
+                min(self.red_tiles[i].x, self.red_tiles[j].x)
+            ) + 1
+            height = (
+                    max(self.red_tiles[i].y, self.red_tiles[j].y) -
+                    min(self.red_tiles[i].y, self.red_tiles[j].y)
+            ) + 1
+            if base * height > max_area:
+                max_area = base * height
+        return max_area
+
 
 class Solution:
     _STANDARD_PATH = Path(__file__).parent / "input.txt"
@@ -9,7 +52,8 @@ class Solution:
             self.lines = file.readlines() if not lines else lines
 
     def first_task(self) -> int:
-        pass
+        floor = TileFloor(self.lines)
+        return floor.max_area
 
 
     def second_task(self) -> int:
