b34b674deae0717ec3ae8664c2d9ea2b324c66fc
Antonino Cangialosi <antonino.cangialosi@agilelab.it>
2025-12-10 10:48:15 +0100

    Submit solution to second part

--- tests/python/day10/test_main.py
+++ tests/python/day10/test_main.py
@@ -1,22 +1,13 @@
 """ This is a template file do not use it directly.
 """
-from dataclasses import dataclass
 from inspect import getsourcefile
 from pathlib import Path
 
 import pytest
 
+from advent_of_code_2025_python.day10.main import Machine
 from src.advent_of_code_2025_python.day10.main import Solution
 
-@dataclass(frozen=True)
-class Machine:
-    light_diagram: str
-    buttons: list[list[int]]
-    joltage_reqs: list[int]
-
-    @property
-    def minimal_buttons_to_turn_on(self) -> tuple[int, list[int]]:
-        pass
 
 @pytest.fixture
 def provide_test_lines() -> list[str]:
@@ -26,36 +17,46 @@ def provide_test_lines() -> list[str]:
         return [line.strip() for line in file.readlines()[example_slice]]
 
 
-def test_first_machine():
-    under_test = Machine(
-        ".##.",
-        [[3], [1,3], [2], [2,3], [0,2], [0,1]],
-        [3,5,4,7]
+@pytest.fixture
+def machine_props():
+    return (
+        (
+            ".##.",
+            [[3], [1,3], [2], [2,3], [0,2], [0,1]],
+            [3,5,4,7],
+        ),
+        (
+            "...#.",
+            [[0,2,3,4], [2,3], [0,4], [0,1,2], [1,2,3,4]],
+            [7,5,12,7,2],
+        ),
+        (
+            ".###.#",
+            [[0,1,2,3,4], [0,3,4], [0,1,2,4,5], [1,2]],
+            [10,11,11,5,10,5],
+        ),
     )
-    expected = 2
-    actual = under_test.minimal_buttons_to_turn_on[0]
-    assert actual == expected
+
+def test_parse(provide_test_lines, machine_props):
+    under_test = Solution.parse
+    for i, line in enumerate(provide_test_lines):
+        expected = machine_props[i]
+        actual = under_test(line)
+        assert actual == expected
 
 
-def test_second_machine():
-    under_test = Machine(
-        "...#.",
-        [[0,2,3,4], [2,3], [0,4], [0,1,2], [1,2,3,4]],
-        [7,5,12,7,2]
-    )
-    expected = 3
-    actual = under_test.minimal_buttons_to_turn_on[0]
-    assert actual == expected
-
-
-def test_third_machine():
-    under_test = Machine(
-        ".###.#",
-        [[0,1,2,3,4], [0,3,4], [0,1,2,4,5], [1,2]],
-        [10,11,11,5,10,5],
-    )
-    expected = 2
-    actual = under_test.minimal_buttons_to_turn_on[0]
+@pytest.mark.parametrize(
+    "machine,expected",
+    (
+        (0, 2),
+        (1, 3),
+        (2, 2),
+    ),
+    ids=["first_machine", "second_machine", "third_machine"]
+)
+def test_machines(machine_props, machine, expected):
+    under_test = Machine(*machine_props[machine])
+    actual = len(under_test.minimal_buttons_to_turn_on())
     assert actual == expected
 
 
