88bf372c1aab7ab7add2095b2fcc9b6eb5df0c7b
Antonino Cangialosi <antonino.cangialosi@agilelab.it>
2025-12-11 07:10:53 +0100

    Submit solution to first part

--- src/advent_of_code_2025_python/day11/main.py
+++ src/advent_of_code_2025_python/day11/main.py
@@ -1,5 +1,43 @@
+import queue
+import re
+from copy import deepcopy
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Counter
+
+import networkx as nx
+
+
+@dataclass(frozen=True)
+class Device:
+    label: str
+    outputs: list[str]
+
+    def from_str(s: str) -> Device:
+        pattern = re.compile(r"\b[a-z]{3}\b")
+        label, output_bit = s.split(":")
+        outputs = re.findall(pattern, output_bit)
+        return Device(label, outputs)
+
+type DeviceLabel = Device.label
+
+class ServerRack:
+    def __init__(
+        self,
+        lines: list[str],
+    ):
+        self.devices = [
+            Device.from_str(line) for line in lines
+        ]
+        self.routes = self._generate_routes()
+
+    def _generate_routes(self) -> nx.DiGraph:
+        routes = nx.DiGraph()
+        for device in self.devices:
+            routes.add_edges_from([(device.label, dest) for dest in device.outputs])
+        return routes
+
+
 
 class Solution:
     _STANDARD_PATH = Path(__file__).parent / "input.txt"
@@ -9,7 +47,16 @@ class Solution:
             self.lines = file.readlines() if not lines else lines
 
     def first_task(self) -> int:
-        pass
+        server = ServerRack(self.lines)
+        return sum(
+            1
+            for _ in nx.all_simple_paths(
+                server.routes,
+                source="you",
+                target="out",
+            )
+        )
+
 
 
     def second_task(self) -> int:
--- tests/python/day11/test_main.py
+++ tests/python/day11/test_main.py
@@ -5,8 +5,7 @@ from pathlib import Path
 
 import pytest
 
-from src.advent_of_code_2025_python.day11.main import Solution
-
+from src.advent_of_code_2025_python.day11.main import Solution, Device
 
 
 @pytest.fixture
@@ -16,6 +15,28 @@ def provide_test_lines() -> list[str]:
         example_slice = slice(21, 31)
         return [line.strip() for line in file.readlines()[example_slice]]
 
+
+@pytest.mark.parametrize(
+    "iteration,label,outputs",
+    (
+        (0, "aaa", ["you", "hhh"],),
+        (1, "you", ["bbb", "ccc"],),
+        (2, "bbb", ["ddd", "eee"],),
+        (3, "ccc", ["ddd", "eee", "fff"],),
+        (4, "ddd", ["ggg"],),
+        (5, "eee", ["out"],),
+        (6, "fff", ["out"],),
+        (7, "ggg", ["out"],),
+        (8, "hhh", ["ccc", "fff", "iii"],),
+        (9, "iii", ["out"],),
+    ),
+)
+def test_should_parse_Device_from_str(provide_test_lines, iteration, label, outputs) -> None:
+    under_test = Device.from_str
+    expected = Device(label, outputs)
+    line_stub = provide_test_lines[iteration]
+    actual = under_test(line_stub)
+    assert actual == expected
 
 
 def test_first_task(provide_test_lines: list[str]) -> None:
